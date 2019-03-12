dee8395054e819c2c7fa11bbba31420d3e2ceb8a
bradenmunn <bradenmunn@yahoo.com>
2019-03-12 18:42:04 -0500

    Updated todo tracker

--- Todo.java
+++ Todo.java
@@ -6,17 +6,43 @@ OU EMAIL____________: bradenmunn@ou.edu
 
 Tasks:
 1. Create the StackHospital and QueueHospital classes
-2. Implement the StackHospital Class
-3. Implement the QueueHospital Class
+2. Implement the StackHospital class
+3. Implement the QueueHospital class
+4. Implement the PriorityQueueHospital class
+5. Do HealthyPerson compareToImpl method
+6. Complete HealthyPerson toString()
+7. Do SickPerson compareToImpl method
+8. Complete SickPerson toString()
+9. Tests for HealthyPerson class
+10. Tests for SickPerson class
+11. Tests for Animal class
+12. Tests for StackHospital class
 
 Estimated Time (minutes):
 1. 5
 2. 15
 3. 20
+4. 15
+5. 5
+6. 5
+7. 10
+8. 5
+9. 25
+10. 20
+11. 10
+12. 20
 
 Actual Time (minutes):
 1. 3
 2. 20
 3. 15
+4. 15
+5. 10
+6. 5
+7. 8
+8. 5
+9. 30
+10. 15
+11. 10
 
 */}
