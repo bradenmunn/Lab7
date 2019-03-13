0312a98d0a0cb8dd4de35a18118d9d43006609e5
bradenmunn <bradenmunn@yahoo.com>
2019-03-12 20:18:59 -0500

    Updated todo tracker (had to create new Todo.java file and copy and paste contents into this one due to file error)

---
+++ src/Todo.java
@@ -0,0 +1,52 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-07
+OU ID_______________: 113483760
+OU EMAIL____________: bradenmunn@ou.edu
+
+Tasks:
+1. Create the StackHospital and QueueHospital classes
+2. Implement the StackHospital class
+3. Implement the QueueHospital class
+4. Implement the PriorityQueueHospital class
+5. Do HealthyPerson compareToImpl method
+6. Complete HealthyPerson toString()
+7. Do SickPerson compareToImpl method
+8. Complete SickPerson toString()
+9. Tests for HealthyPerson class
+10. Tests for SickPerson class
+11. Tests for Animal class
+12. Tests for Person Hospitals
+13. Tests for Animal Hospitals
+
+Estimated Time (minutes):
+1. 5
+2. 15
+3. 20
+4. 15
+5. 5
+6. 5
+7. 10
+8. 5
+9. 25
+10. 20
+11. 10
+12. 30
+13. 20
+
+Actual Time (minutes):
+1. 3
+2. 20
+3. 15
+4. 15
+5. 10
+6. 5
+7. 8
+8. 5
+9. 30
+10. 15
+11. 10
+12. 45
+13. 15
+
+*/}
